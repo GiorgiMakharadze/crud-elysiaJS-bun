37ca4af077f105258e5b01f3e3a78791051d7c96
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2025-02-25 15:03:15 +0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { html } from "@elysiajs/html";
+import staticPlugin from "@elysiajs/static";
+import { Elysia } from "elysia";
+import path from "path";
+
+const VIEWS_PATH = import.meta.dir +"../../public/views";
+
+const app = new Elysia()
+  .use(html())
+  .use(staticPlugin({
+    prefix: '/',
+  }))
+  .get('/', () => Bun.file(path.join(VIEWS_PATH, "home.html")))
+  .get('/add-product', ()=> Bun.file(path.join(VIEWS_PATH, "add-product.html")))
+  .get('/edit', () => Bun.file(path.join(VIEWS_PATH, "edit-product.html")))
+  .listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
