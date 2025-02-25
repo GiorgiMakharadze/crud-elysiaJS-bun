3f8d9196d5ab620a88a91ef86549a7b81d9ce027
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2025-02-25 15:17:04 +0400

    add

--- src/db.ts
+++ src/db.ts
@@ -1,14 +1,24 @@
 import Database from "bun:sqlite";
 import { I } from "elysia/dist/index-3yRrZCrW";
+import { IProduct } from "./interfaces/product.interface";
 
 export class ProductsDatabase {
- private db: Database;
+  private db: Database;
 
- constructor() {
-    this.db = new Database("Products.sqlite")
-    this.createTable()
- }
+  constructor() {
+    this.db = new Database("Products.sqlite");
+    this.createTable();
+  }
 
- createTable() {
-    "CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price NUM, image TEXT)" }
+  createTable() {
+    "CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price NUM, image TEXT)";
+  }
+
+  addProduct(product: IProduct) {
+    return this.db
+      .query(
+        `INSERT INTO products (name, price, image) VALUES (?, ?, ?) RETURNING id`,
+      )
+      .get(product.name, product.price, product.image) as IProduct;
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,30 @@ import staticPlugin from "@elysiajs/static";
 import { Elysia } from "elysia";
 import path from "path";
 import { ProductsDatabase } from "./db";
+import { IProduct } from "./interfaces/product.interface";
 
 const VIEWS_PATH = import.meta.dir + "../../public/views";
 
 const app = new Elysia()
   .use(html())
-  .use(staticPlugin({
-    prefix: '/',
-  }))
-  .decorate('db', new ProductsDatabase())
-  .get('/', () => Bun.file(path.join(VIEWS_PATH, "home.html")))
-  .get('/add-product', ()=> Bun.file(path.join(VIEWS_PATH, "add-product.html")))
-  .get('/edit', () => Bun.file(path.join(VIEWS_PATH, "edit-product.html")))
+  .use(
+    staticPlugin({
+      prefix: "/",
+    }),
+  )
+  .decorate("db", new ProductsDatabase())
+  .get("/", () => Bun.file(path.join(VIEWS_PATH, "home.html")))
+  .get("/add-product", () =>
+    Bun.file(path.join(VIEWS_PATH, "add-product.html")),
+  )
+  .get("/edit", () => Bun.file(path.join(VIEWS_PATH, "edit-product.html")))
+
+  .post("/add-product", ({ db, body, set }) => {
+    db.addProduct(<IProduct>body);
+    set.redirect = "/";
+  })
   .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
